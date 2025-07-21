d3dffbffe4ee5980060819666d7ef4d817564f47
rtrevizo18 <ricardotrevizo18@gmail.com>
2025-07-21 05:40:54 -0500

    feat: Add POST/GET routes for collection insertion + find

--- src/app.js
+++ src/app.js
@@ -1,12 +1,13 @@
 import express from "express";
 import bodyParser from "body-parser";
+import { createProduct, getProducts } from "./mongo.js";
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.post("/products");
+app.post("/products", createProduct);
 
-app.get("/products");
+app.get("/products", getProducts);
 
 app.listen(5001);
--- src/mongo.js
+++ src/mongo.js
@@ -4,12 +4,49 @@ dotenv.config();
 const MONGODB_USERNAME = process.env.MONGODB_USERNAME;
 const MONGODB_PASSWORD = process.env.MONGODB_PSWD;
 
-import MongoClient from "mongodb";
+const PRODUCTS_DATABASE_NAME = "productsDB";
+const PRODUCTS_COLLECTION_NAME = "products";
+
+import { MongoClient } from "mongodb";
 
 const url = `mongodb+srv://${MONGODB_USERNAME}:${MONGODB_PASSWORD}@placescluster0.ydq4ewd.mongodb.net/?retryWrites=true&w=majority&appName=PlacesCluster0`;
 
-const createProduct = async (req, res, next) => {};
+export const createProduct = async (req, res, next) => {
+  const { name, price } = req.body;
+  const newProduct = {
+    name,
+    price,
+  };
+  const client = new MongoClient(url);
 
-const getProducts = async (req, res, next) => {};
+  try {
+    await client.connect();
+    const db = client.db(PRODUCTS_DATABASE_NAME);
+    const result = await db
+      .collection(PRODUCTS_COLLECTION_NAME)
+      .insertOne(newProduct);
+    res.json(newProduct);
+  } catch (err) {
+    return res.status(500).json({ message: "Could not store data." });
+  } finally {
+    await client.close();
+  }
+};
 
-export default { createProduct, getProducts };
+export const getProducts = async (req, res, next) => {
+  const client = new MongoClient(url);
+
+  try {
+    await client.connect();
+    const db = client.db(PRODUCTS_DATABASE_NAME);
+    const result = await db
+      .collection(PRODUCTS_COLLECTION_NAME)
+      .find({})
+      .toArray();
+    res.json(result);
+  } catch (err) {
+    return res.status(500).json({ message: "Could not fetch data." });
+  } finally {
+    await client.close();
+  }
+};
